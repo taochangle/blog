c99e44172074a0ab637be5622357b8f9e4d65aff
sshinttao <taoxin0704@gmail.com>
2016-08-26 09:18:45 +0800

    删除RunTime文件夹，修改config配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,11 +5,12 @@ return array(
 		'DB_HOST'               =>  'localhost', // 服务器地址
 		'DB_NAME'               =>  'blog',          // 数据库名
 		'DB_USER'               =>  'root',      // 用户名
-		'DB_PWD'                =>  '',          // 密码
+		'DB_PWD'                =>  'tao1992',          // 密码
 		'DB_PREFIX'             =>  '',    // 数据库表前缀
 		'DB_PARAMS'          	=>  array(), // 数据库连接参数
 		'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
 		'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
 		'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
 		'SHOW_PAGE_TRACE' =>false,
+		'URL_HTML_SUFFIX' => ''
 );
